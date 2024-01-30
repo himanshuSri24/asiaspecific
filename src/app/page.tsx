4e2e7e91e532ebf4f4206f1ceea4e86008fdd439
gauravsoni1812 <gauravsoni8800@gmail.com>
2024-01-30 18:28:20 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,156 @@
+"use client";
+import "./styles.css";
+import img1 from "../../images/img1.png";
+import img2 from "../../images/Frame 176 (1).png";
 import Image from "next/image";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React from "react";
+import img3 from "../../images/image 21.png";
+
 
 export default function Home() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  React.useEffect(() => {
+    const contentHolderHeight = document.querySelector(".about").offsetHeight;
+    console.log(contentHolderHeight);
+    const imgHolderHeight = window.innerHeight;
+    const additionalScrollHeight = window.innerHeight;
+    const totalBodyHeight =
+      contentHolderHeight + imgHolderHeight + additionalScrollHeight + 20;
+
+    // document.body.style.height = `${totalBodyHeight}px`;
+    document.querySelector(".box1").style.height = `${totalBodyHeight}px`;
+
+    gsap.defaults({
+      duration: 2,
+      ease: "power1.inOut",
+    });
+
+    gsap.to(".img-holder", {
+      rotation: 0,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "power2.inOut",
+      scrollTrigger: {
+        start: "top top",
+        end: "+=200%",
+        scrub: 3,
+      },
+    });
+
+    gsap.to(".img-holder img", {
+      scale: 1,
+      ease: "power2.inOut",
+      scrollTrigger: {
+        start: "top top",
+        end: "+=200%",
+        scrub: 3,
+      },
+    });
+
+    gsap.to(".img-holder", {
+      // ease: "power2.inOut",
+      scale: 1.5,
+      scrollTrigger: {
+        trigger: ".about",
+        start: "top 100%",
+        end: "top 0%",
+        // markers:true,
+        scrub: 3,
+      },
+    });
+
+    gsap
+      .timeline({
+        ease: "power2.inOut",
+        scrollTrigger: {
+          scrub: 1,
+        },
+      })
+      .to(".s1d1p2", {
+        gap: "1000px",
+        scale: 9,
+      });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          scrub: 2,
+        },
+      })
+      .to(".s1d1p1", {
+        scale: 2,
+      });
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="navbar">
+        <Image src={img1} alt="" className="i1" />
+        <div className="a1">
+          <div>Home</div>
+          <div>About Us</div>
+          <div>Product</div>
+          <div>E-Catalogue</div>
+          <div>Contact Us</div>
+        </div>
+      </div>
+      <div className="box1">
+        <div className="s1">
+          <div className="s1d1">
+            <p className="s1d1p1">
+              We build <span>-</span>
+            </p>
+            <div className="s1d1p2">
+              <div className="d1">Luxurious</div>{" "}
+              <div className="d2">Spaces</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="website-content z-0">
+          <div className="img-holder">
+            <Image src={img2} alt="" />
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="about">
+        <div className="about1">
+          <div>
+            <Image src={img3} alt="" className="i2" />
+          </div>
+
+          <div className="about2d2 flex flex-col justify-evenly">
+            <div className="text-[52px] font-[500] leading-[74px] text-[#212121]">
+              About us
+            </div>
+
+            <div className="w-[60px] h-[1px] bg-[#E94D11] relative bottom-2"></div>
+
+            <div>
+              <br />
+              {/* <br /> */}
+              Welcome to ASIA PACIFIC, Morbis premier ceramic manufacturer
+              dedicated to redefining interior and exterior construction
+              materials.
+              <br />
+              <br />
+              We specialize in crafting high-end compact surfaces using top-tier
+              natural raw materials and cutting-edge Italian technology. Our
+              innovative manufacturing process involves subjecting materials to
+              extreme temperatures and pressure, replicating the natural stone
+              formation in a fraction of the time. This results in products with
+              exceptional durability, strength, and aesthetic appeal.
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* circular slider */}
+      <div className="circular-slider">
+        <div className="ellipse "></div>
       </div>
-    </main>
+    </div>
   );
 }
